7192f58d61f18d010f6e37982844ab83cfbe32c5
chmorfop <chmorfop@gmail.com>
2023-02-05 21:05:26 +0200

    -- Initial Changes v22 - Update Parse Coco

--- parse_coco_ic.py
+++ parse_coco_ic.py
@@ -13,53 +13,38 @@ def main(clip_model_type: str):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print('device : {}'.format(device))
     clip_model_name = clip_model_type.replace('/', '_')
-    out_path = f"/content/clipcap/data/coco/clip_feat_{clip_model_name}_train_vqa.pkl"
+    out_path = f"/content/clipcap/data/coco/clip_feat_{clip_model_name}_train_ic.pkl"
     clip_model, preprocess = clip.load(clip_model_type, device=device, jit=False)
-    annotation_path = '/content/drive/MyDrive/Colab Notebooks/COCO/Annotations/VQA_train2014_annotations.json'
-    question_path = '/content/drive/MyDrive/Colab Notebooks/COCO/Annotations/VQA_train2014_questions.json'
-
+    annotation_path = '/content/drive/MyDrive/Colab Notebooks/COCO/Annotations/COCO_captions_val2014.json'
     with open(annotation_path, 'r') as f:
         ann = json.load(f).get('annotations')
-
-    with open(question_path, 'r') as f:
-        questions = json.load(f).get('questions')
 
     print("# %0d QAs loaded from json " % len(ann))
     all_embeddings = []
     all_captions = []
     for i in tqdm(range(len(ann))):
-        temp_ann_question_id = ann[i].get('question_id')
-        temp_q_question_id = questions[i].get('question_id')
         temp_ann_img_id = ann[i].get('image_id')
-        temp_q_img_id = questions[i].get('image_id')
-        if (temp_ann_question_id != temp_q_question_id) and (temp_ann_img_id != temp_q_img_id):
-            raise
-        temp_question = questions[i]
-        temp_ann = ann[i]
-        img_id = temp_ann["image_id"]
+        temp_ann_caption = ann[i].get('caption')
         prepath = "/content/output"
-        filename = f"/train2014/COCO_train2014_{int(img_id):012d}.jpg"
+        filename = f"/train2014/COCO_train2014_{int(temp_ann_img_id):012d}.jpg"
         try:
             image = io.imread(prepath+filename)
         except Exception as e:
-            print(i, img_id)
+            print(i, temp_ann_img_id)
             print(e)
             raise
         image = preprocess(Image.fromarray(image)).unsqueeze(0).to(device)
         with torch.no_grad():
             prefix = clip_model.encode_image(image).cpu()
-
-        temp_dict = {'question': temp_question.get('question'),
-                     'answer': temp_ann.get('multiple_choice_answer'),
+        temp_dict = {
+                     'caption': temp_ann_caption,
                      'clip_embedding': i,
-                     'image_id': temp_ann.get('image_id'),
+                     'image_id': temp_ann_img_id,
                      }
         all_embeddings.append(prefix)
         all_captions.append(temp_dict)
-
     with open(out_path, 'wb') as f:
         pickle.dump({"clip_embedding": torch.cat(all_embeddings, dim=0), "captions": all_captions}, f)
-
     print("%0d embeddings saved." % len(all_embeddings))
     return 0
 
